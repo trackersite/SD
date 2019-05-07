6b8a9df6b709080228240d26eca486b7988a4b89
trackersite <ckorpak@gmail.com>
2019-05-07 16:57:13 +0200

    Update serveur.c

--- serveur.c
+++ serveur.c
@@ -1,69 +1,96 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<sys/types.h>
+#include <stdio.h>
+#include <string.h>   //strlen
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>   //close
+#include <arpa/inet.h>    //close
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <errno.h>
+#include <arpa/inet.h>
+#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros
 #include "serveur.h"
-#include "common.h"
 
-int main(int argc, char const *argv[]) {
-  // adresse socket coté client
-  static struct sockaddr_in addr_client;
-  // adresse socket locale
-  static struct sockaddr_in addr_serveur;
-  // longueur adresse
-  int lg_addr;
-  // socket d'écoute et de service
-  int socket_ecoute, socket_service;
-  // buffer qui contiendra le message reçu
-  char message[TAILLE_BUFFER_SERVEUR];
-  // chaîne reçue du client
-  char *chaine_recue;
-  // chaîne renvoyée au client
-  char *reponse = "bien recu";
-  char *id = "1";
-  // nombre d'octets reçus ou envoyés
-  int nb_octets;
+#define TRUE   1
+#define FALSE  0
+#define MULTICAST_GROUP "226.1.2.3"
+#define MULTICAST_IP "127.0.1.1"
 
-  // création socket TCP d'écoute
-  socket_ecoute = creerSocketTCP(SERVER_PORT_SERVER);
-  if (socket_ecoute == -1) {
-    perror("creation socket");
-    exit(1);
+int main (int argc, char *argv[]) {
+  struct clients_connectes *struct_clients[4];
+
+  int sock, status, socklen;
+  char buffer[TAILLE_BUFFER];
+  char *donnees_client[3];
+  struct sockaddr_in saddr;
+  struct ip_mreq imreq;
+  int identifier = 1;
+  char *client_adresse;
+  char *client_port;
+  char *client_pseudo;
+
+  // set content of struct saddr and imreq to zero
+  memset(&saddr, 0, sizeof(struct sockaddr_in));
+  memset(&imreq, 0, sizeof(struct ip_mreq));
+
+  // open a UDP socket
+  sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
+  if ( sock < 0 )
+    perror("Error creating socket"), exit(0);
+
+  saddr.sin_family = PF_INET;
+  saddr.sin_port = htons(PORT); // listen on port 8080
+  saddr.sin_addr.s_addr = htonl(INADDR_ANY); // bind socket to any interface
+  status = bind(sock, (struct sockaddr *)&saddr, sizeof(struct sockaddr_in));
+
+  if ( status < 0 )
+    perror("Error binding socket to interface"), exit(0);
+
+  imreq.imr_multiaddr.s_addr = inet_addr(MULTICAST_GROUP);
+  imreq.imr_interface.s_addr = INADDR_ANY; // use DEFAULT interface
+
+  // JOIN multicast group on default interface
+  status = setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP,
+             (const void *)&imreq, sizeof(struct ip_mreq));
+
+  while (TRUE) {
+    socklen = sizeof(struct sockaddr_in);
+
+    // receive packet from socket
+    status = recvfrom(sock, buffer, TAILLE_BUFFER, 0,
+                      (struct sockaddr *)&saddr, &socklen);
+    printf(" Received : %s\n", buffer);
+
+    // affecter pseudo
+    client_pseudo = buffer;
+
+    memset(buffer, 0, TAILLE_BUFFER);
+    buffer[0] = identifier + '0';
+    identifier++;
+    printf("New connection, ip is : %s , port : %d \n" , inet_ntoa(saddr.sin_addr) , ntohs(saddr.sin_port));
+
+    sprintf(client_port, "%d", ntohs(saddr.sin_port));
+    // affectation
+    client_adresse = inet_ntoa(saddr.sin_addr);
+    donnees_client[0] = client_adresse;
+    donnees_client[1] = client_port;
+    donnees_client[2] = "Test";
+    // concatenation
+    strcat(*donnees_client, "\n");
+    strcat(*donnees_client, client_port);
+    strcat(*donnees_client, "\n");
+    strcat(*donnees_client, client_pseudo);
+
+    // envoie
+    sendto(sock, *donnees_client, TAILLE_BUFFER, 0,
+                        (struct sockaddr *)&saddr, socklen);
+
   }
 
-  // configuration socket écoute : 5 connexions max en attente
-  if (listen(socket_ecoute, 4) == -1) {
-    perror("erreur listen");
-    exit(1);
-  }
+  // shutdown socket
+  shutdown(sock, 2);
+  // close socket
+  close(sock);
 
-  // on attend la connexion du client
-  lg_addr = sizeof(struct sockaddr_in);
-  socket_service = accept(socket_ecoute,(struct sockaddr *)&addr_client, &lg_addr);
-  if (socket_service == -1) {
-    perror("erreur accept");
-    exit(1);
-  }
-
-  // la connexion est établie, on attend les données envoyées par le client
-  nb_octets = read(socket_service, message, TAILLE_BUFFER_SERVEUR);
-
-  // affichage du message reçu
-  chaine_recue =(char *)malloc(nb_octets * sizeof(char));
-
-  memcpy(chaine_recue, message, nb_octets);
-  printf("%s\n", chaine_recue);
-
-  // on envoie la réponse au client
-  write(socket_service, id, strlen(reponse)+1);
-
-  // on ferme les sockets
-  close(socket_service);
-  close(socket_ecoute);
-
-  return(0);
+  return 0;
 }
