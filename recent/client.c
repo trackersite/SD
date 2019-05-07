a197a374c5230537677fcbd8bfa57d09a3976584
trackersite <ckorpak@gmail.com>
2019-05-07 09:39:45 +0200

    Add files via upload

---
+++ recent/client.c
@@ -0,0 +1,89 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include "client.h"
+
+#define MULTICAST_GROUP "225.1.1.1"
+#define MULTICAST_IP "127.0.1.1"
+
+int main (int argc, char *argv[]) {
+  int sock, status, socklen;
+  char buffer[TAILLE_BUFFER];
+  struct sockaddr_in saddr;
+  struct in_addr iaddr;
+  unsigned char ttl = 3;
+  unsigned char one = 1;
+
+  // set content of struct saddr and imreq to zero
+  memset(&saddr, 0, sizeof(struct sockaddr_in));
+  memset(&iaddr, 0, sizeof(struct in_addr));
+
+  // open a UDP socket
+  sock = socket(PF_INET, SOCK_DGRAM, 0);
+  if ( sock < 0 )
+    perror("Error creating socket"), exit(0);
+
+  saddr.sin_family = PF_INET;
+  saddr.sin_port = htons(0); // Use the first free port
+  saddr.sin_addr.s_addr = htonl(INADDR_ANY); // bind socket to any interface
+  status = bind(sock, (struct sockaddr *)&saddr, sizeof(struct sockaddr_in));
+
+  if ( status < 0 )
+    perror("Error binding socket to interface"), exit(0);
+
+  iaddr.s_addr = INADDR_ANY; // use DEFAULT interface
+
+  // Set the outgoing interface to DEFAULT
+  setsockopt(sock, IPPROTO_IP, IP_MULTICAST_IF, &iaddr,
+             sizeof(struct in_addr));
+
+  // Set multicast packet TTL to 3; default TTL is 1
+  setsockopt(sock, IPPROTO_IP, IP_MULTICAST_TTL, &ttl,
+             sizeof(unsigned char));
+
+  // send multicast traffic to myself too
+  status = setsockopt(sock, IPPROTO_IP, IP_MULTICAST_LOOP,
+                      &one, sizeof(unsigned char));
+
+  // set destination multicast address
+  saddr.sin_family = PF_INET;
+  saddr.sin_addr.s_addr = inet_addr("226.0.0.1");
+  saddr.sin_port = htons(PORT);
+
+  // put some data in buffer
+  strcpy(buffer, "Hello world\n");
+
+  socklen = sizeof(struct sockaddr_in);
+  // receive packet from socket
+  status = sendto(sock, buffer, strlen(buffer), 0,
+                    (struct sockaddr *)&saddr, socklen);
+  if (status < 0) {
+    perror("sendto()");
+  }
+
+  printf("message sent.\n");
+  memset(buffer, 0, TAILLE_BUFFER);
+  recvfrom(sock, buffer, TAILLE_BUFFER, 0,
+                    (struct sockaddr *)&saddr, &socklen);
+  printf("%s\n", buffer);
+
+
+  while (1) {
+    /* code */
+  }
+
+  // shutdown socket
+  shutdown(sock, 2);
+  // close socket
+  close(sock);
+
+  return 0;
+
+}
---
+++ recent/common.h
@@ -0,0 +1,9 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#define PORT 8080
+#define TAILLE_BUFFER 200
+
+int creerSocketTCP();
+
+#endif
