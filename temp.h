6002ca68e1a6d1f7e0a0f419cd72e495dbbfda63
trackersite <ckorpak@gmail.com>
2019-05-10 18:17:16 +0200

    Create temp.h

---
+++ temp.h
@@ -0,0 +1,21 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#define PORT_TCP 8080
+#define PORT_UDP 8888
+#define TAILLE_BUFFER 200
+#define TRUE   1
+#define FALSE  0
+#define MULTICAST_GROUP "226.1.2.3"
+#define MULTICAST_IP "127.0.1.1"
+
+struct clients_connectes {
+  char *pseudo;
+  struct sockaddr_in infos;
+  struct in_addr sin_addr;
+};
+
+int creerSocketTCP();
+
+
+#endif
