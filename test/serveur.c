bb8eee6e8b00a5d7ba9a48923df952e23b11d3d0
trackersite <ckorpak@gmail.com>
2019-05-10 10:48:35 +0200

    Update serveur.c

--- test/serveur.c
+++ test/serveur.c
@@ -9,40 +9,66 @@
 #include <netdb.h>
 #include "serveur.h"
 
-void multicast(int socketUDP_ecoute) {
+int main (int argc, char *argv[]) {
   struct sockaddr_in    localSock, groupSock;
+  struct in_addr        localInterface;
   struct ip_mreq        group;
-  int                   socketUDP;
+  int                   socketUDP, socketTCP, socket_service;
   char                  databuf[TAILLE_BUFFER];
-  int                   identifiant;
-  /*
-   * Create a datagram socket on which to receive.
-   */
+  struct hostent        *host;
+  char                  name[256];
+  struct sockaddr_in    tcp_addr;
+  char*                 donnees_multicast;
+  int                   identifiant = 0;
+  // adresse socket coté client
+  static struct sockaddr_in addr_client;
+  // adresse socket locale
+  static struct sockaddr_in addr_serveur;
+
+  if (socketTCP = creerSocketTCP() <= 0) {
+    printf("Could not create TCP socket.\n");
+    perror("sock()");
+    exit(1);
+  }
+
+  /* recuperer le nom du hote */
+  gethostname(name, 1023);
+  host = gethostbyname(name);
+
+  /* assigner port et l'adresse IP */
+  bzero((char *) &tcp_addr, sizeof(tcp_addr));
+  tcp_addr.sin_family = AF_INET;
+  tcp_addr.sin_port = htons(PORT);
+  memcpy(&tcp_addr.sin_addr.s_addr, host->h_addr, host->h_length);
+
+
+  /**************** PARTIE UDP ****************/
+
   socketUDP = socket(AF_INET, SOCK_DGRAM, 0);
   if (socketUDP < 0) {
     perror("opening datagram socket");
     exit(1);
   }
 
-  /*
-   * Enable SO_REUSEADDR to allow multiple instances of this
-   * application to receive copies of the multicast datagrams.
-   */
+    /*
+     * Enable SO_REUSEADDR to allow multiple instances of this
+     * application to receive copies of the multicast datagrams.
+     */
   {
-    int reuse=1;
+      int reuse=1;
 
-    if (setsockopt(socketUDP, SOL_SOCKET, SO_REUSEADDR,
-                   (char *)&reuse, sizeof(reuse)) < 0) {
+      if (setsockopt(socketUDP, SOL_SOCKET, SO_REUSEADDR,
+                     (char *)&reuse, sizeof(reuse)) < 0) {
       perror("setting SO_REUSEADDR");
       close(socketUDP);
       exit(1);
     }
   }
 
-  /*
-   * Bind to the proper port number with the IP address
-   * specified as INADDR_ANY.
-   */
+ /*
+  * Bind to the proper port number with the IP address
+  * specified as INADDR_ANY.
+  */
   memset((char *) &localSock, 0, sizeof(localSock));
   localSock.sin_family = AF_INET;
   localSock.sin_port = htons(PORT);
@@ -62,74 +88,52 @@ void multicast(int socketUDP_ecoute) {
    */
   group.imr_multiaddr.s_addr = inet_addr(MULTICAST_GROUP);
   group.imr_interface.s_addr = inet_addr(MULTICAST_IP);
-  if (setsockopt(socketUDP_ecoute, IPPROTO_IP, IP_ADD_MEMBERSHIP,
-                 (char *)&group, sizeof(group)) < 0) {
+  if (setsockopt(socketUDP, IPPROTO_IP, IP_ADD_MEMBERSHIP,
+                   (char *)&group, sizeof(group)) < 0) {
     perror("adding multicast group");
     close(socketUDP);
     exit(1);
   }
 
-  /*
-   * Read from the socket.
-   */
-  if (read(socketUDP, databuf, sizeof(databuf)) < 0) {
-    perror("reading datagram message");
-    close(socketUDP);
-    exit(1);
-  } else {
-    printf("Multicast received from client : \n%s\n", databuf);
-  }
-  memset((char *) &groupSock, 0, sizeof(groupSock));
-  groupSock.sin_family = AF_INET;
-  groupSock.sin_addr.s_addr = inet_addr(MULTICAST_GROUP);
-  groupSock.sin_port = htons(PORT);
-
-  if (sendto(socketUDP, databuf, sizeof(databuf), 0,
-             (struct sockaddr*)&groupSock,
-             sizeof(groupSock)) < 0)
-  {
-    perror("sending datagram message");
-  } else {
-    printf("datagram sent to TCP Server.\n");
-  }
-}
-
-int main (int argc, char *argv[]) {
-  struct sockaddr_in    localSock, groupSock;
-  struct in_addr        localInterface;
-  struct ip_mreq        group;
-  int                   socketUDP, socketTCP;
-  char                  databuf[TAILLE_BUFFER];
-  struct hostent        *host;
-  char                  name[256];
-  struct sockaddr_in    tcp_addr;
-
-  if (socketTCP = creerSocketTCP() <= 0) {
-    printf("Could not create TCP socket.\n");
-    perror("sock()");
+  // configuration socket écoute : 5 connexions max en attente
+  if (listen(socketTCP, 5) == -1) {
+    perror("erreur listen");
     exit(1);
   }
 
-  /* recuperer le nom du hote */
-  gethostname(name, 1023);
-  host = gethostbyname(name);
+  int lg_addr = sizeof(struct sockaddr_in);
 
-  /* assigner port et l'adresse IP */
-  bzero((char *) &tcp_addr, sizeof(tcp_addr));
-  tcp_addr.sin_family = AF_INET;
-  tcp_addr.sin_port = htons(PORT);
-  memcpy(&tcp_addr.sin_addr.s_addr, host->h_addr, host->h_length);
+    // configuration socket écoute : 5 connexions max en attente
+    if (listen(socketTCP, 5) == -1) {
+      perror("erreur listen");
+      exit(1);
+    }
 
-  /*
-   * Create a datagram socket on which to receive.
-   */
-  socketUDP = socket(AF_INET, SOCK_DGRAM, 0);
-  if (socketUDP < 0) {
-    perror("opening datagram socket");
-    exit(1);
-  }
+    // on attend la connexion du client
+    lg_addr = sizeof(struct sockaddr_in);
 
-  multicast(socketUDP);
+    while (1) {
+      socket_service = accept(socket_ecoute, (struct sockaddr *)&addr_client, &lg_addr);
+
+      if (socket_service == -1) {
+        perror("erreur accept");
+        exit(1);
+      }
+
+      // la connexion est établie, on attend les données envoyées par le client
+      nb_octets = read(socket_service, message, TAILLEBUF);
+
+      // affichage du message reçu
+      chaine_recue = (char *)malloc(nb_octets * sizeof(char));
+      memcpy(chaine_recue, databuf, nb_octets);
+      printf("%s\n", databuf);
+      // on envoie la réponse au client
+      write(socket_service, databuf, strlen(databuf)+1);
+    }
+
+    // on ferme les sockets
+    close(socket_service);
+    close(socket_ecoute);
 
   close(socketUDP);
   close(socketTCP);
